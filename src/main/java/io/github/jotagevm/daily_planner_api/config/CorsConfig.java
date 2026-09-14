898262d20b11deac52ec17ef9e97aea814c58178
JotaGeVM <joogvm@gmail.com>
2026-09-13 22:38:49 -0300

    feat: permitir CORS para o dominio do Vercel

--- src/main/java/io/github/jotagevm/daily_planner_api/config/CorsConfig.java
+++ src/main/java/io/github/jotagevm/daily_planner_api/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173")
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173", "https://daily-planner-web-sandy.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS").allowedHeaders("*")
                 .allowCredentials(true);
     }
